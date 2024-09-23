7f18424cf4ce85abbdeb81e326d33730ce9fb9ba
linchengzzz <1139489076@qq.com>
2024-09-23 10:21:13 +0800

    fix: del r3 bio

--- src/app/fund/Fund.tsx
+++ src/app/fund/Fund.tsx
@@ -47,11 +47,6 @@ export default function Fund() {
         link: 'https://biohackerdao.com/',
       },
       {
-        title: 'R3 Bio',
-        description: 'Wholebody replacement',
-        image: <img className="w-[8.9375rem] mobile:w-[4.6875rem]" src="/imgs/investments/r3.webp" alt="r3" />,
-      },
-      {
         title: 'Mito Health',
         description: 'AI Powered Concierge Doctor',
         image: <img className="w-22.5 mobile:w-12" src="/imgs/investments/mito.jpg" alt="mito" />,
@@ -77,7 +72,7 @@ export default function Fund() {
             Access To cutting-edge products, exclusive events, and a network of innovators
           </p>
         )}
-        <div className="mt-12 grid w-full grid-cols-6 gap-7.5 px-18 mobile:mt-6 mobile:grid-cols-2 mobile:gap-0 mobile:px-0 mobile:pb-10">
+        <div className="mt-12 grid w-full grid-cols-5 gap-7.5 px-18 mobile:mt-6 mobile:grid-cols-2 mobile:gap-0 mobile:px-0 mobile:pb-10">
           {funds.map((item) => (
             <div
               onClick={() => handleFundClick(item)}
