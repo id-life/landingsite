9897e476a1387bc0f836a1dbe6855307b4bfa623
linchengzzz <1139489076@qq.com>
2024-09-24 20:10:57 +0800

    chore: geo size

--- src/app/intervention/Intervention.tsx
+++ src/app/intervention/Intervention.tsx
@@ -7,7 +7,7 @@ import { OrbitControls } from '@react-three/drei';
 
 export default function Intervention() {
   return (
-    <div id={NAV_LIST[5].id} className="page-height mt-56 flex flex-col p-12 mobile:mt-56 mobile:p-0 mobile:pt-9">
+    <div id={NAV_LIST[5].id} className="mt-56 px-12 mobile:mt-56 mobile:p-0 mobile:pt-9">
       <h2 className="page-title">Regional Intervention Center</h2>
       <div className="mt-9 flex gap-7 mobile:mt-5 mobile:grid mobile:grid-cols-1 mobile:gap-3">
         <div className="processes-clip px-3.5 py-2 text-xl/5 font-medium capitalize text-white mobile:w-fit mobile:py-1 mobile:text-sm/5">
@@ -23,39 +23,40 @@ export default function Intervention() {
           &nbsp;Clinic and medical staff
         </div>
       </div>
-      <div className="flex-1"></div>
-      <Canvas
-        shadows
-        dpr={[1, 2]}
-        camera={{ position: [0, 0, 10], far: 1000 }}
-        gl={{ powerPreference: 'high-performance', depth: false }}
-      >
-        <pointLight position={[-10, -10, -10]} intensity={1} />
-        <ambientLight intensity={0.4} />
-        <spotLight
-          castShadow
-          angle={0.3}
-          penumbra={1}
-          position={[0, 10, 20]}
-          intensity={5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-        />
-        <spotLight
-          castShadow
-          angle={0.3}
-          penumbra={1}
-          position={[0, 10, 20]}
-          intensity={5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-        />
-        <Suspense fallback={null}>
-          <Geo />
-        </Suspense>
-        <Effects />
-        <OrbitControls autoRotate enableDamping enableZoom={false} target={[0, 0, 0]} />
-      </Canvas>
+      <div className="page-height mt-12">
+        <Canvas
+          shadows
+          dpr={[1, 2]}
+          camera={{ position: [0, 0, 10], far: 1000 }}
+          gl={{ powerPreference: 'high-performance', depth: false }}
+        >
+          <pointLight position={[-10, -10, -10]} intensity={1} />
+          <ambientLight intensity={0.4} />
+          <spotLight
+            castShadow
+            angle={0.3}
+            penumbra={1}
+            position={[0, 10, 20]}
+            intensity={5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <spotLight
+            castShadow
+            angle={0.3}
+            penumbra={1}
+            position={[0, 10, 20]}
+            intensity={5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <Suspense fallback={null}>
+            <Geo />
+          </Suspense>
+          <Effects />
+          <OrbitControls autoRotate enableDamping enableZoom={false} target={[0, 0, 0]} />
+        </Canvas>
+      </div>
     </div>
   );
 }
