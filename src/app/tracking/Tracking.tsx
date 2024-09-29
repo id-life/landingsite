9b3f2d8eb63a39417785971fbb103b0467c54b70
yusixian <cosine_yu@qq.com>
2024-09-29 16:32:39 +0800

    fix: ui text

--- src/app/tracking/Tracking.tsx
+++ src/app/tracking/Tracking.tsx
@@ -26,7 +26,7 @@ const Tracking_List: TrackingItem[] = [
 export default function Tracking() {
   return (
     <div id={NAV_LIST[4].id} className="mt-[16.625rem] p-12 mobile:mt-56 mobile:p-0 mobile:pt-9">
-      <h2 className="page-title">Tracking Center</h2>
+      <h2 className="page-title">Experience Center</h2>
       <p className="font-migrena text-2xl/12 font-bold uppercase mobile:mt-1.5 mobile:text-sm/5 mobile:capitalize">Singapore</p>
       <div className="mt-12 mobile:mt-6">
         <div className="flex h-112 w-3/4 gap-5 mobile:h-auto mobile:w-full mobile:flex-col mobile:gap-4">
