78176c2a71bdfb5be83a666b12e10845bb4dfc68
yusixian <cosine_yu@qq.com>
2024-10-18 14:21:09 +0800

    feat: vision decorations

--- src/app/vision/Vision.tsx
+++ src/app/vision/Vision.tsx
@@ -2,6 +2,9 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import CenterLogo from '@/components/gl/CenterLogo';
 import { DragonModel } from '@/components/gl/model/dragon/DragonModel';
+import VisionDecorationRightBottomSVG from '@/../public/svgs/vision/vision-decoration-1.svg?component';
+import VisionDecorationBottomScrollSVG from '@/../public/svgs/vision/vision-decoration-2.svg?component';
+import VisionDecorationCircleSVG from '@/../public/svgs/vision/vision-decoration-3.svg?component';
 
 export default function Vision() {
   return (
@@ -14,6 +17,27 @@ export default function Vision() {
           </Suspense>
         </Canvas>
       </div>
+      <div className="absolute bottom-10 left-10 flex select-none items-center gap-1.5">
+        <VisionDecorationRightBottomSVG className="h-12" />
+        <p className="whitespace-pre-wrap font-poppins text-base/4.5 font-bold uppercase text-black">
+          {'Our goal is to increase global\nawareness of longevity'}
+        </p>
+      </div>
+      <img
+        src="/imgs/vision-decoration-text.png"
+        draggable={false}
+        className="absolute bottom-24 left-1/2 w-[35.625rem] -translate-x-1/2"
+      />
+      {/* pieces of decoration */}
+      <div className="absolute left-10 top-[calc(50%_-_14rem)] h-2 w-6 bg-black" />
+      <div className="absolute left-10 top-[calc(50%_+_14rem)] h-2 w-9 bg-black" />
+      <div className="absolute right-10 top-[calc(50%_+_14rem)] h-4 w-4 bg-black" />
+      <div className="absolute right-[13.5rem] top-[calc(50%_-_14rem)] h-4 w-4 bg-black" />
+      <div className="absolute bottom-7 left-1/2 flex -translate-x-1/2 items-center gap-2">
+        <VisionDecorationBottomScrollSVG className="h-12" />
+        <p className="font-migrena text-xl/6 font-bold uppercase">SCROLL</p>
+      </div>
+      <VisionDecorationCircleSVG className="absolute right-10 top-[calc(50%_-_14rem)] h-4 w-4" />
     </div>
   );
 }
