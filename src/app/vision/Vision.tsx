ea99537103cf3fa45e1adbce27700c7ddd908fea
yusixian <cosine_yu@qq.com>
2024-11-26 13:11:34 +0800

    fix: vh 2 dvh

--- src/app/vision/Vision.tsx
+++ src/app/vision/Vision.tsx
@@ -24,10 +24,10 @@ export default function Vision() {
         <img
           src="/imgs/vision-decoration-text.png"
           draggable={false}
-          className="absolute left-1/2 top-[calc(100vh_-_8rem)] w-[35.625rem] -translate-x-1/2 text-gray-800 mobile:top-[calc(100vh_-_5.625rem)] mobile:w-[18.75rem] mobile:-translate-y-1/2"
+          className="absolute left-1/2 top-[calc(100dvh_-_8rem)] w-[35.625rem] -translate-x-1/2 text-gray-800 mobile:top-[calc(100dvh_-_5.625rem)] mobile:w-[18.75rem] mobile:-translate-y-1/2"
           alt="TIL UNLIMITED HUMAN HEALTHY LIFESPAN"
         />
-        <div className="absolute left-1/2 top-[calc(100vh_-_5rem)] flex -translate-x-1/2 items-center gap-2 mobile:top-[calc(100vh_-_3.375rem)] mobile:rounded-lg mobile:bg-white/50 mobile:px-1.5 mobile:py-2 mobile:backdrop-blur-2xl">
+        <div className="absolute left-1/2 top-[calc(100dvh_-_5rem)] flex -translate-x-1/2 items-center gap-2 mobile:top-[calc(100dvh_-_3.375rem)] mobile:rounded-lg mobile:bg-white/50 mobile:px-1.5 mobile:py-2 mobile:backdrop-blur-2xl">
           <VisionDecorationBottomScrollSVG className="h-12 w-10 mobile:h-6 mobile:w-5" />
           <p className="font-migrena text-xl/6 font-bold uppercase mobile:text-xs/5">SCROLL</p>
         </div>
