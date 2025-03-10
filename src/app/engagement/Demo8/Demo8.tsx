99661c98f30632047ad296210a43553b3e00aece
linchengzzz <1139489076@qq.com>
2025-03-10 17:13:56 +0800

    fix: build error

--- src/app/engagement/Demo8/Demo8.tsx
+++ src/app/engagement/Demo8/Demo8.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { Slider } from '@/app/engagement/Demo8/slider';
+// import { useEffect } from 'react';
+// import { Slider } from '@/app/engagement/Demo8/slider';
 
 export default function Demo8() {
-  useEffect(() => {
-    new Slider();
-  }, []);
+  // useEffect(() => {
+  //   new Slider();
+  // }, []);
 
   return (
     <div className="js-slider w-[67.5rem] h-[37.5rem]">
