0600b0e79ad8a8ef30caa18b764c685f6013037d
linchengzzz <1139489076@qq.com>
2025-04-01 15:28:14 +0800

    fix: scroll behavior

--- src/app/twin/_components/mobile/SwitchAnatomyCamera.tsx
+++ src/app/twin/_components/mobile/SwitchAnatomyCamera.tsx
@@ -31,7 +31,7 @@ export default function SwitchAnatomyCamera() {
   return (
     <div id="switch-anatomy-camera" className="absolute bottom-32 left-1/2 z-20 -translate-x-1/2">
       {currentModelType === ModelType.Anatomy && (
-        <div className="flex gap-7 bg-white/10 p-2 backdrop-blur">
+        <div className="flex gap-4 bg-white/10 p-2 backdrop-blur">
           {currentModel === PredictionModel.M0 && (
             <>
               <AnatomyM01
