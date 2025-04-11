05b91d2a15d3caa63966da9f779184e94fac682c
yusixian <i@cosine.ren>
2025-04-11 14:10:05 +0800

    fix: fixed ui opacity

--- src/components/common/FixedUI.tsx
+++ src/components/common/FixedUI.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { glLoadedAtom } from '@/atoms/geo';
 import { useIsMobile } from '@/hooks/useIsMobile';
-import { useAtomValue } from 'jotai';
 import MobileFixedUI from './MobileFixedUI';
 import PCFixedUI from './PCFixedUI';
+import { cn } from '@/utils';
+import { useAtomValue } from 'jotai';
+import { globalLoadedAtom } from '@/atoms/geo';
 
 export default function FixedUI() {
   const isMobile = useIsMobile();
-  // const glLoaded = useAtomValue(glLoadedAtom); // 重要
-  // if (!glLoaded) return null;
-  return isMobile ? <MobileFixedUI /> : <PCFixedUI />;
+  const globalLoaded = useAtomValue(globalLoadedAtom);
+  return <div className={cn(globalLoaded ? 'opacity-100' : 'opacity-0')}>{isMobile ? <MobileFixedUI /> : <PCFixedUI />}</div>;
 }
