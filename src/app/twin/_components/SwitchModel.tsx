b27db236f6b892fb9dd283e4f08ae6b9c96600fe
linchengzzz <1139489076@qq.com>
2025-04-28 17:02:04 +0800

    feat: model ui animate

--- src/app/twin/_components/SwitchModel.tsx
+++ src/app/twin/_components/SwitchModel.tsx
@@ -39,18 +39,21 @@ export default function SwitchModel() {
       <div className="relative cursor-pointer p-2" onClick={() => handleSwitchModel(PredictionModel.M0)}>
         <SelectBorderSVG
           className={clsx(
-            'absolute left-0 top-0 h-auto w-full',
+            'absolute left-0 top-0 h-auto w-full animate-scale',
             currentModel === PredictionModel.M0 ? 'stroke-red-600' : 'stroke-black',
           )}
         />
-        {currentModel === PredictionModel.M0 && <SelectSVG className="absolute -right-7 top-6" />}
+        {currentModel === PredictionModel.M0 && (
+          <SelectSVG className="absolute -right-10 bottom-0 top-0 my-auto animate-move-right" />
+        )}
         <img src="/imgs/twin/avatar-model0.png" alt="" />
         <div className="flex-center absolute inset-2 left-2 top-2 bg-red-600/20 font-semibold text-white opacity-0 backdrop-blur-sm transition-all duration-150 hover:opacity-100">
           ORG
         </div>
       </div>
       <img className="mx-auto" src="/svgs/twin/avatar-divider.svg" alt="" />
-      <div className="grid gap-2 border-2 border-black p-2">
+      <div className="relative grid gap-2 p-2">
+        <div className="absolute inset-0 animate-pulse-30 border-2 border-black"></div>
         <div
           className={clsx('relative cursor-pointer', currentModel === PredictionModel.M1 ? 'bg-[#D7BAC4]' : 'bg-[#B0B6C1]')}
           onClick={() => handleSwitchModel(PredictionModel.M1)}
@@ -60,6 +63,9 @@ export default function SwitchModel() {
           <div className="flex-center absolute inset-0 left-0 top-0 bg-red-600/20 font-semibold text-white opacity-0 backdrop-blur-sm transition-all duration-150 hover:opacity-100">
             C01
           </div>
+          {currentModel === PredictionModel.M1 && (
+            <SelectSVG className="absolute -right-12 bottom-0 top-0 my-auto animate-move-right" />
+          )}
         </div>
         <div
           className={clsx('relative cursor-pointer', currentModel === PredictionModel.M2 ? 'bg-[#D7BAC4]' : 'bg-[#B0B6C1]')}
@@ -70,6 +76,9 @@ export default function SwitchModel() {
           <div className="flex-center absolute inset-0 left-0 top-0 bg-red-600/20 font-semibold text-white opacity-0 backdrop-blur-sm transition-all duration-150 hover:opacity-100">
             C02
           </div>
+          {currentModel === PredictionModel.M2 && (
+            <SelectSVG className="absolute -right-12 bottom-0 top-0 my-auto animate-move-right" />
+          )}
         </div>
         <div
           className={clsx('relative cursor-pointer', currentModel === PredictionModel.M3 ? 'bg-[#D7BAC4]' : 'bg-[#B0B6C1]')}
@@ -80,6 +89,9 @@ export default function SwitchModel() {
           <div className="flex-center absolute inset-0 left-0 top-0 bg-red-600/20 font-semibold text-white opacity-0 backdrop-blur-sm transition-all duration-150 hover:opacity-100">
             C03
           </div>
+          {currentModel === PredictionModel.M3 && (
+            <SelectSVG className="absolute -right-12 bottom-0 top-0 my-auto animate-move-right" />
+          )}
         </div>
       </div>
     </div>
