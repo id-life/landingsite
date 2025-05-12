be0d71a7436f04738271165faf4bbbda3bf5aa6b
Lo <l709937065@gmail.com>
2025-05-12 16:06:21 +0800

    fix: update comments and improve styling in Portfolio and PortfolioItem

--- src/app/portfolio/_components/PortfolioItem.tsx
+++ src/app/portfolio/_components/PortfolioItem.tsx
@@ -17,14 +17,11 @@ const PortfolioItem = memo(
       <div
         ref={ref}
         onClick={onClick}
-        className={cn(
-          'mobile:flex-center relative h-60 w-[23.75rem] cursor-pointer pt-3 text-foreground mobile:h-[30svh] mobile:w-[100dvw] mobile:flex-col mobile:pt-0',
-          className,
-        )}
+        className={cn('relative h-[17.5rem] w-[23.75rem] cursor-pointer pt-3 text-foreground', className)}
       >
-        <div className="flex h-20 items-center justify-center mobile:h-[3.875rem]">{image}</div>
-        <div className="mt-4 text-center font-semibold">
-          <h4 className="fund-title flex-center gap-2.5 font-oxanium text-base/6 mobile:text-xl/6">
+        <div className="flex h-20 items-center justify-center">{image}</div>
+        <div className="mt-7.5 text-center font-semibold">
+          <h4 className="fund-title flex-center gap-2.5 font-oxanium text-xl/6 tracking-normal">
             <img src="/svgs/select.svg" className="title-selected w-5.5 rotate-180 opacity-0" alt="" />
             {title}
             <img src="/svgs/select.svg" className="title-selected w-5.5 opacity-0" alt="" />
