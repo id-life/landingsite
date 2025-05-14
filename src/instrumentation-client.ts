e9ecd79fe709e06e2968ed80c37da90ba3a65496
linchengzzz <1139489076@qq.com>
2025-05-14 16:20:35 +0800

    fix: cancel switching page disable scroll

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,17 +1,16 @@
-// This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://da52a0eda21c3287f29a63f9ae3f04af@o4509309166616576.ingest.us.sentry.io/4509309184180224',
 
-  integrations: [Sentry.replayIntegration()],
+  integrations: [Sentry.replayIntegration(), Sentry.browserTracingIntegration(), Sentry.browserProfilingIntegration()],
 
   tracesSampleRate: 0.2,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 0.1,
+  profilesSampleRate: 0.5,
+  // tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
   debug: false,
   enabled: process.env.NODE_ENV !== 'development',
 });
