5f749f9f3a77d5fb78649e3e32b00a76839a7d47
linchengzzz <1139489076@qq.com>
2025-05-19 12:10:48 +0800

    chore: type

--- src/components/twin/Loader.tsx
+++ src/components/twin/Loader.tsx
@@ -7,17 +7,17 @@ import ProgressLoader from '@/components/gl/ProgressLoader';
 
 export default function Loader() {
   const { progress, active, loaded } = useProgress();
-  const setGLLoaded = useSetAtom(twinGlLoadedAtom);
+  const setTwinLoaded = useSetAtom(twinGlLoadedAtom);
   const setCurrentModel = useSetAtom(currentModelAtom);
   // 设置全局加载完成
   useEffect(() => {
     if (!active) {
-      setGLLoaded(true);
+      setTwinLoaded(true);
     }
     return () => {
-      setGLLoaded(true);
+      setTwinLoaded(true);
     };
-  }, [active, setCurrentModel, setGLLoaded]);
+  }, [active, setCurrentModel, setTwinLoaded]);
 
   return <ProgressLoader progress={progress.toFixed(2)} />;
 }
