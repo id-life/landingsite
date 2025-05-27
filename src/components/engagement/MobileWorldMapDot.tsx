5f2a115e99efcfe6c73aea5646edfa866226d2f4
yusixian <i@cosine.ren>
2025-05-27 13:24:46 +0800

    style: update font size

--- src/components/engagement/MobileWorldMapDot.tsx
+++ src/components/engagement/MobileWorldMapDot.tsx
@@ -120,7 +120,7 @@ export function MobileWorldMapDotPoint({
           </svg>
         </div>
         {/* 标签 */}
-        <motion.p className="-ml-1.5 flex flex-col items-start whitespace-nowrap font-oxanium text-xl/6 font-semibold capitalize text-white">
+        <motion.p className="-ml-1.5 flex flex-col items-start whitespace-nowrap font-oxanium text-base/5 font-semibold capitalize text-white">
           {label ? `${label}, ` : ''}
           {country}
           {isActive && (
