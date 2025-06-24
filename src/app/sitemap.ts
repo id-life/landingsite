7461b91e41db36c8ee41cc03d3e854302c264216
linchengzzz <1139489076@qq.com>
2025-06-24 17:20:39 +0800

    feat: add new geo list

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,7 @@
 import type { MetadataRoute } from 'next';
 
-const newsId = [
-  2, 3, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 19, 20, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-  44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 79,
-  83, 84, 85, 86, 87, 88, 91, 92, 93, 94, 99, 100,
-];
-
 export default function sitemap(): MetadataRoute.Sitemap {
-  const sitemap: MetadataRoute.Sitemap = [
+  return [
     {
       url: 'https://www.id.life/',
       lastModified: new Date(),
@@ -16,12 +10,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
     {
       url: 'https://www.id.life/news',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.6,
-    },
-    {
-      url: 'https://www.id.life/news/57',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.6,
@@ -45,13 +33,4 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
     },
   ];
-  newsId.forEach((id) => {
-    sitemap.push({
-      url: `https://www.id.life/news/${id}`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    });
-  });
-  return sitemap;
 }
