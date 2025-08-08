eed06ad36b732dd3b77a96885fde372da13cce4a
linchengzzz <1139489076@qq.com>
2025-08-08 16:15:02 +0800

    fix: style

---
+++ src/app/(home)/_components/Style.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import React from 'react';
+
+import 'swiper/css';
+import '@/styles/webgl.css';
+import '@/styles/loading.css';
+import '@/styles/components/character-relation.css';
+
+export default function Style() {
+  return (
+    <style jsx global>{`
+      :root {
+        font-size: 16px;
+      }
+
+      body {
+        overscroll-behavior: none;
+        background: var(--background);
+      }
+
+      @media screen and (min-width: 1px) {
+        :root {
+          font-size: 0.8333vw;
+        }
+      }
+
+      @media screen and (max-width: 768px) {
+        :root {
+          font-size: 16px;
+        }
+
+        body {
+          height: 100%;
+          overflow: hidden;
+        }
+      }
+    `}</style>
+  );
+}
