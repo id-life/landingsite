21ffdf4ecd47819ee622409e085ab86a5a5e40d7
linchengzzz <1139489076@qq.com>
2025-08-15 11:04:19 +0800

    chore: add link text

--- src/app/news/_components/NewsArticle1.tsx
+++ src/app/news/_components/NewsArticle1.tsx
@@ -7,15 +7,15 @@ export default function NewsArticle1() {
       <h1>Immortal Dragons is a purpose-driven longevity fund headquartered in Biopolis, Singapore.</h1>
       <img src="https://cdn.id.life/id-life-cover-2.webp" alt="" />
       <NewsContent />
-      <Link href="/news" />
-      <Link href="/podcast" />
-      <Link href="/podcast?c=id" />
-      <Link href="/podcast?c=lt" />
+      <Link href="/podcast">Podcast</Link>
+      <Link href="/podcast?c=id">Immortal Dragons</Link>
+      <Link href="/podcast?c=lt">Long talk</Link>
+      <Link href="/about">About</Link>
+      <Link href="/news">News</Link>
       <Link href="/portfolio" />
       <Link href="/spectrum" />
       <Link href="/digitaltwin" />
       <Link href="/value" />
-      <Link href="/about" />
     </div>
   );
 }
