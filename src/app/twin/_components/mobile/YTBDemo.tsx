e54badef480b071ff74ed125534d6924c20a3e45
linchengzzz <1139489076@qq.com>
2025-08-18 15:37:05 +0800

    fix: open link pause

--- src/app/twin/_components/mobile/YTBDemo.tsx
+++ src/app/twin/_components/mobile/YTBDemo.tsx
@@ -1,17 +1,19 @@
-import YoutubeSVG from '@/../public/svgs/twin/youtube.svg?component';
 import React from 'react';
+import { useSetAtom } from 'jotai';
 import { useGA } from '@/hooks/useGA';
 import { GA_EVENT_NAMES } from '@/constants/ga';
 import { DigitalTwinYoutubeLink } from '@/constants/links';
+import YoutubeSVG from '@/../public/svgs/twin/youtube.svg?component';
+import { AUDIO_DISPATCH, audioControlsAtom } from '@/atoms/audio-player';
 
 export default function YTBDemo() {
   const { trackEvent } = useGA();
+  const dispatch = useSetAtom(audioControlsAtom);
 
   const handleClick = (e: React.MouseEvent) => {
     e.stopPropagation();
-    trackEvent({
-      name: GA_EVENT_NAMES.DT_DEMO,
-    });
+    dispatch({ type: AUDIO_DISPATCH.PAUSE });
+    trackEvent({ name: GA_EVENT_NAMES.DT_DEMO });
   };
   return (
     <div id="ytb-demo" className="absolute left-5 top-[22rem] z-20 grid cursor-pointer gap-5">
@@ -22,11 +24,10 @@ export default function YTBDemo() {
         className="group relative flex h-[28px] items-center"
         onClick={handleClick}
       >
-        <div className="flex-center gap-2 text-sm font-medium group-hover:text-red-600">
-          <YoutubeSVG className="w-4 fill-black group-hover:fill-red-600" />
+        <div className="flex-center gap-2 text-sm font-medium">
+          <YoutubeSVG className="w-4 fill-black" />
           DEMO
         </div>
-        {/* <RightSVG className="fill-black stroke-black group-hover:fill-red-600 group-hover:stroke-red-600 ml-2" /> */}
         <img src="/imgs/twin/arrow.webp" alt="" className="ml-1 h-2 w-3" />
       </a>
     </div>
