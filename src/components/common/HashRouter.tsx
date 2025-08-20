a6c15d84d13216da765f86e094f0f17e8cbdc501
yusixian <i@cosine.ren>
2025-08-20 12:15:41 +0800

    fix: update UI opacity handling in HashRouter

--- src/components/common/HashRouter.tsx
+++ src/components/common/HashRouter.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { useCallback, useEffect } from 'react';
-import gsap from 'gsap';
-import { useAtomValue } from 'jotai';
 import { globalLoadedAtom } from '@/atoms/geo';
 import { eventBus } from '@/components/event-bus/eventBus';
 import { MessageType } from '@/components/event-bus/messageType';
-import { useNavigation } from '@/hooks/useNavigation';
-import { useMobileNavigation } from '@/hooks/useMobileNavigation';
-import { useIsMobile } from '@/hooks/useIsMobile';
 import { NAV_LIST } from '@/components/nav/nav';
+import { useIsMobile } from '@/hooks/useIsMobile';
+import { useMobileNavigation } from '@/hooks/useMobileNavigation';
+import { useNavigation } from '@/hooks/useNavigation';
+import gsap from 'gsap';
+import { useAtomValue } from 'jotai';
+import { useCallback, useEffect } from 'react';
 
 export default function HashRouter() {
   const globalLoaded = useAtomValue(globalLoadedAtom);
@@ -53,18 +53,20 @@ export default function HashRouter() {
     // Handle initial page load
     handleHashNavigation();
     handlePathnameNavigation();
-
-    // fixed ui opacity, see DragonModel triggerFadeInAnimation
-    if (isMobile) {
-      const mobileNav = document.querySelector('#mobile-nav');
-      const mobileElement = document.querySelector('#mobile-fixed-ui');
-      if (mobileNav) gsap.set(mobileNav, { opacity: 1 });
-      if (mobileElement) gsap.set(mobileElement, { opacity: 1 });
-    } else {
-      const nav = document.querySelector('#nav');
-      const element = document.querySelector('#pc-fixed-ui');
-      if (nav) gsap.set(nav, { opacity: 1 });
-      if (element) gsap.set(element, { opacity: 1 });
+    const { pathname } = window.location;
+    if (pathname !== '/') {
+      // fixed ui opacity, see DragonModel triggerFadeInAnimation
+      if (isMobile) {
+        const mobileNav = document.querySelector('#mobile-nav');
+        const mobileElement = document.querySelector('#mobile-fixed-ui');
+        if (mobileNav) gsap.set(mobileNav, { opacity: 1 });
+        if (mobileElement) gsap.set(mobileElement, { opacity: 1 });
+      } else {
+        const nav = document.querySelector('#nav');
+        const element = document.querySelector('#pc-fixed-ui');
+        if (nav) gsap.set(nav, { opacity: 1 });
+        if (element) gsap.set(element, { opacity: 1 });
+      }
     }
 
     window.addEventListener('hashchange', handleHashNavigation);
