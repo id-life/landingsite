dc338e8b3b4f665479708bb2c580a533533206c5
AprilNEA <github@sku.moe>
2025-08-21 15:23:19 +0800

    feat: disable page scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,12 @@ export const metadata: Metadata = {
       },
     ],
   },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
