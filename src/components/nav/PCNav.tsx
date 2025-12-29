2b99c426885e1c59a645cee451ad58e70f927f3b
yusixian <i@cosine.ren>
2025-12-29 13:51:07 +0800

    fix: PCNav click

--- src/components/nav/PCNav.tsx
+++ src/components/nav/PCNav.tsx
@@ -60,11 +60,13 @@ export default function PCNav() {
     <div
       id="nav"
       className={cn(
-        'fixed left-0 top-0 z-50 flex w-full items-center gap-15 p-10 text-foreground opacity-0 transition-opacity duration-300 mobile:hidden',
+        'pointer-events-none fixed left-0 top-0 z-50 flex w-full items-center gap-15 p-10 text-foreground opacity-0 transition-opacity duration-300 mobile:hidden',
       )}
     >
-      <Logo />
-      <div className={cn('main-nav-links flex gap-8 text-sm font-semibold')}>
+      <div className="pointer-events-auto">
+        <Logo />
+      </div>
+      <div className={cn('main-nav-links pointer-events-auto flex gap-8 text-sm font-semibold')}>
         {NAV_LIST.map((item) => (
           <Link
             href={item.href}
@@ -85,7 +87,7 @@ export default function PCNav() {
       <div id="subscribe-btn" className="flex h-12 flex-1 justify-end mobile:h-auto mobile:items-center">
         <div
           onClick={onSubscribeClick}
-          className="group relative flex h-10 w-33 cursor-pointer items-center justify-center text-sm font-semibold uppercase duration-300 hover:stroke-red-600 hover:text-red-600 mobile:h-8 mobile:w-24 mobile:text-xs/5"
+          className="group pointer-events-auto relative flex h-10 w-33 cursor-pointer items-center justify-center text-sm font-semibold uppercase duration-300 hover:stroke-red-600 hover:text-red-600 mobile:h-8 mobile:w-24 mobile:text-xs/5"
         >
           <SubscribeBorderSVG className="absolute left-0 top-0 size-full stroke-foreground duration-300 group-hover:stroke-red-600" />
           Subscribe
